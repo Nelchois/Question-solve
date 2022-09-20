c5cbc33ba58fcb2cf78faf3f6194942cfe735505
Nelchois <reiethd@gmail.com>
2022-09-20 23:58:35 +0900

    Update BOJ.py
    10818

---
+++ BOJ10818.py
@@ -0,0 +1,5 @@
+import sys
+A = int(sys.stdin.readline())
+B = map(int, sys.stdin.readline().split(' '))
+print(min(B))
+print(max(B))
--- BOJ1110.py
+++ BOJ1110.py
@@ -1,13 +1,34 @@
-num_int = int(input())
+import sys
+num_int = int(sys.stdin.readline())
 check_num = num_int
 counter = 0
+
 while True:
+    a = num_int%10 
+    b = num_int//10
+    c = num_int*10
+    
     if num_int < 10:
-        num_int = (num_int*10) + num_int
+        if (counter > 0) and (num_int == check_num):
+            print(counter)
+            break
+        num_int = c + num_int
         counter += 1
-    elif (counter == 0) and (check_num == num_int) and (num_int > 10):
-        num_int = ((num_int%10)*10) + (((num_int//10) + (num_int%10))%10)
+        
+    elif (num_int >= 10) and (check_num != num_int):
+        num_int = (a*10) + ((a+b)%10)
         counter += 1
-    elif (counter != 0) and (check_num == num_int):
+        continue
+        
+    elif (check_num == num_int) and (counter == 0) and (num_int >= 10):
+        num_int = (a*10) + ((a+b)%10)
+        counter += 1
+        continue
+        
+    elif check_num == num_int:
+        print(counter)
+        break
+    
+    else:
         print(counter)
         break
