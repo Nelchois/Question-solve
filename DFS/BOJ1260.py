438331e4dab28d92f8acc1a0cf529e0b3f24b39d
Nelchois <reiethd@gmail.com>
2022-10-07 22:54:04 +0900

    Update BOJ code
    Update DFS

--- DFS/BOJ1260.py
+++ DFS/BOJ1260.py
@@ -1,34 +1,101 @@
-from operator import index
 import sys
 from collections import deque
-num, route, start = map(int, sys.stdin.readline().rstrip().split(' '))
-map_dic = dict()
-for i in range(route * 2):
+''' prototype
+num, route, point = map(int, sys.stdin.readline().rstrip().split(' '))
+map_list = []
+for i in range(route):
     node, way = map(int, sys.stdin.readline().rstrip().split(' '))
-    if node in map_dic:
-        map_dic[f'{node}'].append(way)
-    elif node not in map_dic:
-        map_dic[f'{node}'] = [way]
-    if way in map_dic:
-        map_dic[f'{way}'].append(node)
-    elif way not in map_dic:
-        map_dic[f'{way}'] = [node]
-def DFS(num, map_dic, point):
+    map_list.append((node, way))
+    map_list.append((way, node))
+map_dict = defaultdict(list)
+for k, v in map_list:
+    map_dict[k].append(v)
+
+def DFS(num, map_dict, point):
     ck_point = []
     map_deque = deque()
-    while len(ck_point) != num:
-        map_deque.append(point)
-        now = map_deque.pop()
-        if now not in ck_point:
-            ck_point.append(now)
-        elif now in ck_point:
-            now = ck_point[-1]
-        ro = map_dic[f'{now}']
-        if min(ro) not in ck_point:
-            now = min(ro)        
-        elif min(ro) in ck_point and max(ro) not in ck_point:
-            now = max(ro)
+    while True:
+        if len(ck_point) == len(map_dict):
+            break
+        if len(map_deque) == 0:
+            map_deque.append(point)
+            now = map_deque.pop()
+            if now not in ck_point:
+                ck_point.append(now)
+            map_deque.append(max(map_dict[now]))
+            map_deque.append(min(map_dict[now]))
+        elif len(map_deque) != 0:
+            now = map_deque.pop()
+            if now not in ck_point:
+                ck_point.append(now)
+            way1 = max(map_dict[now])
+            way2 = min(map_dict[now])
+            if way1 in ck_point and way2 in ck_point:
+                continue
+            elif way1 in ck_point and way2 not in ck_point:
+                map_deque.append(way2)
+            elif way1 not in ck_point and way2 in ck_point:
+                map_deque.append(way1)
     return ck_point
-#a = DFS(num, map_dic, start)
-#print(len(a))
-print(map_dic)
+def BFS(num, map_list, point):
+    ck_point2 = []
+    map_deque = deque()
+    while True:
+        if len(ck_point2) == len(map_list):
+            break
+        if len(map_deque) == 0:
+            map_deque.append(point)
+            now = map_deque.popleft()
+            map_deque.append(min(map_list[now]))
+            map_deque.append(max(map_list[now]))
+            ck_point2.append(now)
+        elif len(map_deque) != 0:
+            now = map_deque.popleft()
+            if now not in ck_point2:
+                ck_point2.append(now)
+            elif now in ck_point2: 
+                continue   
+            if len(map_list[now]) > 1:
+                way1 = max(map_list[now])
+                way2 = min(map_list[now])
+                if way1 in ck_point2 and way2 not in ck_point2:
+                    map_deque.append(way2)
+                elif way2 in ck_point2 and way1 not in ck_point2:
+                    map_deque.append(way1)
+            elif len(map_list[now]) == 1:
+                way = map_list[now]
+                if way not in ck_point2:
+                    map_deque.append(way)
+    return ck_point2
+print(*DFS(num, map_dict, point))
+print(*BFS(num, map_dict, point))
+'''
+n, m, v = map(int, sys.stdin.readline().rstrip().split())
+
+adj = [[0 for i in range(n + 1)] for j in range(n + 1)]
+
+for i in range(m):
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    adj[a][b] = 1
+    adj[b][a] = 1
+
+def dfs(v, hist, adj):
+    hist.append(v)
+    for i in range(1, n + 1):
+        if adj[v][i] and i not in hist:
+            hist = dfs(i, hist, adj)
+    return hist
+
+def bfs(v, adj):
+    q = [v]
+    hist = [v]
+    while q:
+        now = q.pop(0)
+        for i in range(1, n + 1):
+            if adj[now][i] and i not in hist:
+                q.append(i)
+                hist.append(i)
+    return hist
+
+print(*dfs(v, [], adj))
+print(*bfs(v, adj))
--- DP/BOJ1149.py
+++ DP/BOJ1149.py
@@ -1,25 +1,7 @@
 import sys
-from collections import deque
-house = int(sys.stdin.readline())
-cost = []
-color = 0
-R = deque()
-G = deque()
-B = deque()
-route = {}
-for i in range(house):
-    r, g, b = map(int, sys.stdin.readline().rstrip().split(' '))
-    R.append(r)
-    G.append(g)
-    B.append(b)
-def in_color():
-    for _ in range(house):
-        r1 = R.popleft()
-        r2 = R.popleft()
-        g1 = G.popleft()
-        g2 = G.popleft()
-        b1 = B.popleft()
-        b2 = B.popleft()
-        route['12'] = r1 + g2
-        route['13'] = r1 + b2
-        route[]
+num, route, point = map(int, sys.stdin.readline().rstrip().split(' '))
+map_list = dict()
+for i in range(route):
+    node, way = map(int, sys.stdin.readline().rstrip().split(' '))
+    map_list[list[node][0]] += [list[way][0]]
+print(map_list)
