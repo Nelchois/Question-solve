9824ea473ac9cd002efa632b5e16cf58436cdbd8
Nelchois <reiethd@gmail.com>
2022-10-10 23:59:30 +0900

    Update BOj2581
    change code

--- BOJ_Basic_math_2/BOJ2581.py
+++ BOJ_Basic_math_2/BOJ2581.py
@@ -3,15 +3,29 @@ m = int(sys.stdin.readline().rstrip())
 n = int(sys.stdin.readline().rstrip())
 num_list = [i for i in range(m, n+1) if m <= i <= n]
 ans = []
-for i in len(num_list):
-    ck = num_list.pop()
-    if ck == 2 or 3 or 5 or 7:
-        ans.append(ck)
+for i in (num_list):
+    ck = [2, 3, 5, 7]
+    if i <= 7:
+        if i in ck:
+            ans.append(i)    
     else:
-    if len(ans) == 0:
+        for j in range(4):
+            if i%ck[j] == 0:
+                break
+            elif i%ck[j] != 0 and j == 3:
+                ans.append(i)
+if len(ans) == 0:
         print(-1)
-    else:
+else:
+    if ans[0]**2 > ans[-1]:
         print(sum(ans))
         print(min(ans))
 
-     
+    elif ans[0]**2 < ans[-1]:
+        for i in range(len(ans)):
+            try:
+                ans.remove(ans[i]**2)
+            except:
+                continue
+        print(sum(ans))
+        print(min(ans))
