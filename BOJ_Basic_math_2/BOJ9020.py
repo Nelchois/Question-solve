64c9d91d1b57c450d87f29a58e2c80f2311e7063
Nelchois <reiethd@gmail.com>
2022-10-15 00:13:25 +0900

    renew code BOJ9020.py
    change code

--- BOJ_Basic_math_2/BOJ9020.py
+++ BOJ_Basic_math_2/BOJ9020.py
@@ -5,24 +5,14 @@ for n in range(2, 101):
     if num_bol[n] == True:
         for m in range(n+n, 10001, n):
             num_bol[m] = False
-number = [_ for _ in range(2, 10001) if num_bol[_] == True]
 for i in range(case):
     ck_num = int(sys.stdin.readline())
     count = 0
     find_list = []
     min_list = []
     ans = []
-    while ck_num/2 > number[count]:
-        back_num = ck_num - number[count]
-        if number[count] <= back_num:
-            find_list.append([number[count], back_num])
-        if count < len(number):
-            count += 1
-    for j in range(len(find_list)):
-        A, B = find_list[j][0], find_list[j][1]
-        if B - A < 0:
-            min_list.append(A - B) 
-        else:
-            min_list.append(B - A)
-    idx = min_list.index(min(min_list))
-    print(*find_list[idx])
+    m = int(ck_num/2)
+    for i in range(m):
+        if (num_bol[m - i] == True) and (num_bol[m + i] == True):
+            print(m-i, m+i)
+            break
