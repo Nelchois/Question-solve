22515dbe0746d4e6923f5f5444731d3f9de00ca8
Nelchois <reiethd@gmail.com>
2022-10-26 23:53:34 +0900

    Update BOJ CODe
    Update code

--- RECURSION/BOJ24060.py
+++ RECURSION/BOJ24060.py
@@ -1,11 +1,12 @@
 import sys
 n, k = map(int, sys.stdin.readline().rstrip().split(' '))
 num_list = list(map(int, sys.stdin.readline().rstrip().split(' ')))
+counter = []
 def merge_sort(arr):
     if len(arr) < 2:
         return arr
 
-    mid = len(arr) // 2
+    mid = (len(arr) + 1) // 2
     low_arr = merge_sort(arr[:mid])
     high_arr = merge_sort(arr[mid:])
 
@@ -14,14 +15,29 @@ def merge_sort(arr):
     while l < len(low_arr) and h < len(high_arr):
         if low_arr[l] < high_arr[h]:
             merged_arr.append(low_arr[l])
+            counter.append(low_arr[l])
             l += 1
         
         else:
             merged_arr.append(high_arr[h])
+            counter.append(high_arr[h])
             h += 1
-            
-    merged_arr += low_arr[l:]
-    merged_arr += high_arr[h:]
+    
+    while l < len(low_arr):
+        merged_arr.append(low_arr[l])
+        counter.append(low_arr[l])
+        l += 1
+
+    while h < len(high_arr):
+        merged_arr.append(high_arr[h])
+        counter.append(high_arr[h])
+        h += 1
+
     return merged_arr
+
 a = merge_sort(num_list)
-print(a)
+if len(counter) >= k:
+    print(counter[k-1])
+else:
+    print(-1)
+    
