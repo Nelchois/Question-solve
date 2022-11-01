25a296ba338d1275e004bf0f4636f84a7b1da04d
Nelchois <reiethd@gmail.com>
2022-11-02 03:23:56 +0900

    UPdate BOJ code
    Update COde

--- BRUTE_FORCE/BOJ2231.py
+++ BRUTE_FORCE/BOJ2231.py
@@ -4,9 +4,18 @@ int_num = int(str_num)
 length = len(str_num)
 ans = []
 def ck_number(num):
-    if num == int_num - length*9:
+    if num <= 0 or num < int_num - length*9:
         return num
     else:
-        num
+        ck_str = str(num)
+        num_list = []
+        for i in range(len(ck_str)):
+            num_list.append(int(ck_str[i]))
+        if num + sum(num_list) == int_num:
+            ans.append(num)
         return ck_number(num - 1)
-
+a = ck_number(int_num)
+if len(ans) > 0:
+    print(min(ans))
+elif len(ans) == 0:
+    print(0)
