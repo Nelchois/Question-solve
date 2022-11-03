5e7a2ca23973789e24ae72b7ecea2044ed637ef1
Nelchois <reiethd@gmail.com>
2022-11-03 20:15:26 +0900

    Update BOJ code
    Udpate BOJ code

---
+++ BRUTE_FORCE/BOJ1018.py
@@ -0,0 +1,10 @@
+import sys
+n, m = map(int, sys.stdin.readline())
+chess = []
+for i in range(n):
+    chess.append([sys.stdin.readline().rstrip()])
+for j in range(len(chess)):
+    if chess[j][0] == 'w':
+        chess[j]
+
+
