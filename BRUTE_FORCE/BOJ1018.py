0d2729fae3154d238efd2f9b4d5b9db442958bbf
Nelchois <reiethd@gmail.com>
2022-11-08 23:53:51 +0900

    Update BOJ CODE

--- BRUTE_FORCE/BOJ1018.py
+++ BRUTE_FORCE/BOJ1018.py
@@ -1,10 +1,76 @@
 import sys
-n, m = map(int, sys.stdin.readline())
+n, m = map(int, sys.stdin.readline().rstrip().split(' '))
 chess = []
+idx_1 = [[0 for j in range(m)] for i in range(n)]
+idx_2 = [[0 for j in range(m)] for i in range(n)]
+ans = []
+for _ in range(n):
+    chess.append(sys.stdin.readline().rstrip())
+'''
 for i in range(n):
-    chess.append([sys.stdin.readline().rstrip()])
-for j in range(len(chess)):
-    if chess[j][0] == 'w':
-        chess[j]
+    for j in range(m):
+        if i//2 == 0:
+            if j//2 == 0:
+                if chess[i][j] == "B":
+                    idx_1[i][j] = 0
+                else:
+                    idx_1[i][j] = 1
+            else:
+                if chess[i][j] == "W":
+                        idx_1[i][j] = 0
+                else:
+                    idx_1[i][j] = 1
+        else:
+            if j//2 == 0:
+                if chess[i][j] == "W":
+                    idx_1[i][j] = 0
+                else:
+                    idx_1[i][j] = 1
+            else:
+                if chess[i][j] == "B":
+                        idx_1[i][j] = 0
+                else:
+                    idx_1[i][j] = 1
+for k in range(n):
+    for l in range(m):
+        ck_line = []
+        if k + 8 <= n and l + 8 <= m:
+            for _ in range(k, k + 8):
+                ck_line.append(sum(idx_1[k][l : l + 8]))
+            ans.append(sum(ck_line))
+'''
+for i in range(n):
+    for j in range(m):
+        if i//2 == 0:
+            if j//2 == 0:
+                if chess[i][j] == "W":
+                    idx_2[i][j] = 0
+                else:
+                    idx_2[i][j] = 1
+            else:
+                if chess[i][j] == "B":
+                        idx_2[i][j] = 0
+                else:
+                    idx_2[i][j] = 1
+        else:
+            if j//2 == 0:
+                if chess[i][j] == "B":
+                    idx_2[i][j] = 0
+                else:
+                    idx_2[i][j] = 1
+            else:
+                if chess[i][j] == "W":
+                        idx_2[i][j] = 0
+                else:
+                    idx_2[i][j] = 1
+for k in range(n):
+    for l in range(m):
+        ck_line = []
+        if k + 8 <= n and l + 8 <= m:
+            for _ in range(k, k + 8):
+                ck_line.append(sum(idx_2[k][l : l + 8]))
+            ans.append(sum(ck_line))
+
+print(ans)
 
 
