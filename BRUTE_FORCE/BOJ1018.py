608e5504a8155a63edfc7dc87ec85a5b4e6a9300
Nelchois <reiethd@gmail.com>
2022-11-09 23:58:00 +0900

    Update BOJ CODE
    Update CODE

--- BRUTE_FORCE/BOJ1018.py
+++ BRUTE_FORCE/BOJ1018.py
@@ -9,8 +9,8 @@ for _ in range(n):
 '''
 for i in range(n):
     for j in range(m):
-        if i//2 == 0:
-            if j//2 == 0:
+        if i%2 == 0:
+            if j%2 == 0:
                 if chess[i][j] == "B":
                     idx_1[i][j] = 0
                 else:
@@ -21,7 +21,7 @@ for i in range(n):
                 else:
                     idx_1[i][j] = 1
         else:
-            if j//2 == 0:
+            if j%2 == 0:
                 if chess[i][j] == "W":
                     idx_1[i][j] = 0
                 else:
@@ -41,8 +41,8 @@ for k in range(n):
 '''
 for i in range(n):
     for j in range(m):
-        if i//2 == 0:
-            if j//2 == 0:
+        if i%2 == 0:
+            if j%2 == 0:
                 if chess[i][j] == "W":
                     idx_2[i][j] = 0
                 else:
@@ -53,7 +53,7 @@ for i in range(n):
                 else:
                     idx_2[i][j] = 1
         else:
-            if j//2 == 0:
+            if j%2 == 0:
                 if chess[i][j] == "B":
                     idx_2[i][j] = 0
                 else:
@@ -65,12 +65,11 @@ for i in range(n):
                     idx_2[i][j] = 1
 for k in range(n):
     for l in range(m):
-        ck_line = []
-        if k + 8 <= n and l + 8 <= m:
-            for _ in range(k, k + 8):
-                ck_line.append(sum(idx_2[k][l : l + 8]))
-            ans.append(sum(ck_line))
+        if k + 8 <= n and l + 8 <= m: 
+            st_idx = idx_2[k][l]
+            
+        else: continue
 
-print(ans)
+print(*idx_2, sep= '\n')
 
 
