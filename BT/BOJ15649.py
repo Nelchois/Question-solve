9c69d6db9a81f5603b57b0e9b126d3439c940bef
Nelchois <reiethd@gmail.com>
2022-11-24 04:56:54 +0900

    Update BOJ CODE
    Update BT

--- BT/BOJ15649.py
+++ BT/BOJ15649.py
@@ -1,6 +1,5 @@
 import sys
 n, m = map(int, sys.stdin.readline().rstrip().split(' '))
-number_list = [i for i in range(1, n + 1)]
 number_line = []
 def make_line():
     if len(number_line) == m:
--- BT/BOJ15650.py
+++ BT/BOJ15650.py
@@ -2,13 +2,21 @@ import sys
 n, m = map(int, sys.stdin.readline().rstrip().split(' '))
 number_list = [i for i in range(1, n + 1)]
 ans = []
+sort_ck = []
 def number():
     if len(ans) == m:
-        print(ans)
+        print(' '.join(map(str, ans)))
         return
-    for num in number_list:
-        if num not in ans:
-            ans.append(num)
-            number()
-            ans.pop()
+    for num in range(1, n + 1):
+        if len(ans) == 0:
+            if num not in ans:
+                ans.append(num)
+                number()
+                ans.pop()
+        else:
+            if num not in ans and num > ans[-1]:
+                ans.append(num)
+                number()
+                ans.pop()
 number()
+
