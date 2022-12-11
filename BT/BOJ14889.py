894589afa2f76e58529696eab10b2ec79f728492
Nelchois <reiethd@gmail.com>
2022-12-12 03:01:26 +0900

    Update BOJ CODE
    Update BT

--- BT/BOJ14889.py
+++ BT/BOJ14889.py
@@ -1,29 +1,39 @@
 import sys
-from itertools import combinations
 n = int(sys.stdin.readline())
-people = [i for i in range(n)]
+people = [i for i in range(1, n + 1)]
 status = []
 for st in range(n):
     status.append(list(map(int, sys.stdin.readline().rstrip().split(' '))))
 team = []
+score = []
 board = []
+ans = []
 def make_team():
-    for set in combinations(people, 3):
-        left = list(set)
-        right = [i for i in people if i not in left]
-        team.append(left)
-        team.append(right)
-    return
-def cal():
-    for idx in range(len(team)):
-        if idx%2 == 0:
-            for i in combinations(team[idx], 2):
-                q = list(i)
-                board.append(status[q[0]][q[1]])
-        if idx%2 != 0:        
-            for i in combinations(team[idx], 2):
-                q = list(i)
-                board.append(status[q[0]][q[1]])
-            
+    if len(team) == n//2:
+        for num_1 in team:
+            for num_2 in team:
+                if num_1 != num_2:
+                    score.append(status[num_1 - 1][num_2 - 1])
+                    if len(score) == (n//2)*(n//2 - 1) :
+                        board.append(sum(score))
+                        score.clear()
+        return
+    for i in people:
+        if len(team) == 0:
+            if i not in team:
+                team.append(i)
+                make_team()
+                team.pop()
+        elif len(team) != 0: 
+            if i not in team and i > team[-1]:
+                team.append(i)
+                make_team()
+                team.pop()
+
 make_team()
-print(team)
+for i in range(len(board)//2):
+    dif = board.pop(0) - board.pop()
+    if dif < 0:
+        dif *= -1
+    ans.append(dif)
+print(min(ans))
