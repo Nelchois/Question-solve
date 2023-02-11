1ce1449b877a8a15d980734a47989af3a5abeb95
Nelchois <reiethd@gmail.com>
2023-02-11 15:56:34 +0900

    Feat: Problem solve
    programmers 70129
    
    Done

---
+++ PRO70129.py
@@ -0,0 +1,19 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/70129
+def solution(s):
+    board = ''
+    count = 0
+    answer = []
+    while True:
+        if s == '1':
+            break
+        else:
+            for num in s:
+                if num == '1':
+                    board += num
+            ans = len(s) - len(board)
+            answer.append(ans)
+            s = bin(len(board))[2:]
+            board = ''
+            count += 1
+    ans = [count, sum(answer)]
+    return ans
