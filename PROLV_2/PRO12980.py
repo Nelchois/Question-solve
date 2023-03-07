629b3b9ec536740864787518a8255cd900354604
Nelchois <reiethd@gmail.com>
2023-03-07 23:58:42 +0900

    Feat: problem solve
    programmers 12980
    
    Done

---
+++ PROLV_2/PRO12980.py
@@ -0,0 +1,11 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/12980
+def solution(n):
+    ans = 0
+    while n != 1:
+        if n%2 == 0:
+            n //= 2
+        else:
+            n -= 1
+            ans += 1
+    ans += 1
+    return ans
