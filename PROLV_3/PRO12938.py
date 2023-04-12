403bba1d68df6aa6aabf8436c10b2df2e3cf9191
Nelchois <reiethd@gmail.com>
2023-04-12 22:23:27 +0900

    Feat: problem solve
    programmers 12938
    
    Done

---
+++ PROLV_3/PRO12938.py
@@ -0,0 +1,22 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/12938
+def solution(n, s):
+    answer = []
+    if s == 1:
+        if n == 1:
+            return [1]
+        else:
+            return [-1]
+    if n > s:
+        return [-1]
+    share, left = divmod(s, n)
+    for i in range(n):
+        answer.append(share)
+    idx = -1
+    while left != 0:
+        answer[idx] += 1
+        left -= 1
+        if idx * (-1) == len(answer):
+            idx = -1
+        else:
+            idx -= 1
+    return answer
