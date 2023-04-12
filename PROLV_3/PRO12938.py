06542c6ae4ba63fafd0e737df57cb1fb267e62e4
Nelchois <reiethd@gmail.com>
2023-04-12 22:44:14 +0900

    Feat: problem solve
    programmers 12938
    
    Update code

--- PROLV_3/PRO12938.py
+++ PROLV_3/PRO12938.py
@@ -15,8 +15,5 @@ def solution(n, s):
     while left != 0:
         answer[idx] += 1
         left -= 1
-        if idx * (-1) == len(answer):
-            idx = -1
-        else:
-            idx -= 1
+        idx -= 1
     return answer
