0ebf805b86dfa9d1bd00642ebdfc18b36679b201
Nelchois <reiethd@gmail.com>
2023-05-16 20:54:40 +0900

    Feat: problem solve
    programmers 43164
    
    Done

--- PROLV_3/PRO43164.py
+++ PROLV_3/PRO43164.py
@@ -1,41 +1,35 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/43164
 board = ["ICN"]
-
 def dfs(airport, route, air, ans_list):
-    if len(board) == len(airport):
-        print(board)
-        #ans_list.append(tuple(board))
-        return
-    now = list(route[air])
+    now = list(airport[air])
     for i in now:
-        if airport[i] == False:
-            airport[i] = True
+        r = air + i
+        if route[r] != 0:
+            route[r] -= 1
             board.append(i)
-            dfs(airport, route, i, ans_list)
+            if sum(list(route.values())) == 0:
+                ans_list.append(tuple(board))
+            if i in airport:
+                dfs(airport, route, i, ans_list)
             board.pop()
-            airport[i] = False
-
+            route[r] += 1
 def solution(tickets):
     answer = []
     airport = dict()
     route = dict()
     for ro in tickets:
-        air1 = ro[0]
-        air2 = ro[1]
+        air1, air2 = ro[0], ro[1]
+        air = air1 + air2
+        if air not in route:
+            route[air] = 1
+        else:
+            route[air] += 1
         if air1 not in airport:
-            airport[air1] = False
-        if air2 not in airport:
-            airport[air2] = False
-        if air1 not in route:
-            route[air1] = {air2}
+            airport[air1] = {air2}
         else:
-            route[air1].add(air2)
-        if air2 not in route:
-            route[air2] = {air1}
-        else:
-            route[air2].add(air1)
+            airport[air1].add(air2)
     ans_list = []
-    airport["ICN"] = True
     dfs(airport, route, "ICN", ans_list)
-    #print(ans_list)
+    ans_list.sort(reverse = True)
+    answer = list(ans_list.pop())
     return answer
