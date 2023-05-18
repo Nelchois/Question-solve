3ac9654db77d60dd31396fd8e4441095857bfc37
Nelchois <reiethd@gmail.com>
2023-05-18 23:25:54 +0900

    Feat: problem solve
    programmers 64062
    
    Done

---
+++ PROLV_3/PRO64062.py
@@ -0,0 +1,29 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/64062#
+from collections import deque
+def solution(stones, k):
+    answer = 200000001
+    que = deque()
+    if len(stones) <= 1:
+        if len(stones) == 1:
+            answer = stones.pop()
+        return answer
+    for i, stone in enumerate(stones):
+        if len(que) == 0:
+            que.append([stone, i])
+            continue
+        while que:
+            right = que.pop()
+            if stone < right[0]:
+                que.append(right)
+                break
+        que.append([stone, i])
+        while que:
+            left = que.popleft()
+            if i + 1 - k <= left[1] <= i + k:
+                que.appendleft(left)
+                break
+        if i + 1 >= k:
+            m = que.popleft()
+            answer = min(answer, m[0])
+            que.appendleft(m)
+    return answer
