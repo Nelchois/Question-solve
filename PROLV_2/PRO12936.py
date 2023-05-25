8f7ed3bd778a68ab1bd70296636d25e62680b1d8
Nelchois <reiethd@gmail.com>
2023-05-25 11:56:47 +0900

    Feat: problem solve
    programmers 12936
    
    Done

---
+++ PROLV_2/PRO12936.py
@@ -0,0 +1,38 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/12936
+def total(n):
+    if n > 1:
+        return n*total(n-1)
+    elif n == 1:
+        return 1
+    
+def dfs(board, n, visit):
+    if len(board) == n:
+        ans_list.append(list(board))
+        return
+    for i in range(1, n + 1):
+        if not visit[f'{i}']:
+            board.append(i)
+            visit[f'{i}'] = True
+            dfs(board, n, visit)
+            board.pop()
+            visit[f'{i}'] = False
+
+def order(o, k, n):
+    t = total(n - o)
+    num = 1
+    while k > t:
+        k -= t
+        num += 1
+    return num, k
+
+def solution(n, k):
+    answer = []
+    num_list = [i for i in range(1, n + 1)]
+    o = 1
+    while o < n: 
+        st, k = order(o, k, n)
+        st = num_list.pop(st - 1)
+        answer.append(st)
+        o += 1
+    answer.append(num_list[0])
+    return answer
